8958c0015d9cd85469c57d9b7833430f6064cbea
NicolaLassandro <n.lassandro4@studenti.uniba.it>
2022-05-07 19:43:07 +0200

    Aggiunto attributo parolaSegreta e relativi metodi di accesso

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -1,5 +1,21 @@
 package it.uniba.app;
 
+/**
+ * Questa classe è di tipo ' '.
+ * Si occupa di 
+ */
 public class Wordle {
     
+    // ATTRIBUTI
+    private String parolaSegreta;
+
+    // METODI
+    public String getParolaSegreta(){
+        return new String(parolaSegreta);
+    }
+    
+    public void setParolaSegreta(String p){
+        parolaSegreta = p;
+    }
+
 }
