d0138970d5c137995670be0c4369f0523a402b73
adrianafrascella <a.frascella11@studenti.uniba.it>
2022-05-10 10:10:51 +0200

    Introdotto controllo getParolaSegreta in iniziaPartita -Giocatore

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class Giocatore {
     public static void iniziaPartita(){
-        Partita p = new Partita(Wordle.getMaxTentativi(), Wordle.getParolaSegreta()); 
-        System.out.println("Una nuova partita sta iniziando!");
-        /** stampare l'help */
-        /** stampare griglia vuota */
-        Scanner sc = new Scanner(System.in);
-        while(p.getTentativiEffettuati() < p.getMaxTentativi()){
-            System.out.print("Inserisci il tuo tentativo:\n>");
-            String parolaTentata = sc.nextLine().toUpperCase();
-            while(parolaTentata.length() != p.getParola().length()){
-                System.out.print("La parola da inserire deve avere lunghezza " + p.getParola().length() + ":\n>");
-                parolaTentata= sc.nextLine().toUpperCase();
+        if(Wordle.getParolaSegreta() != null){            
+            Partita p = new Partita(Wordle.getMaxTentativi(), Wordle.getParolaSegreta()); 
+            System.out.println("Una nuova partita sta iniziando!");
+            /** stampare l'help */
+            /** stampare griglia vuota */
+            Scanner sc = new Scanner(System.in);
+            while(p.getTentativiEffettuati() < p.getMaxTentativi()){
+                System.out.print("Inserisci il tuo tentativo:\n>");
+                String parolaTentata = sc.nextLine().toUpperCase();
+                while(parolaTentata.length() != p.getParola().length()){
+                    System.out.print("La parola da inserire deve avere lunghezza " + p.getParola().length() + ":\n>");
+                    parolaTentata= sc.nextLine().toUpperCase();
+                }
+                /** inserire tentativo*/
+                p.setTentativiEffettuati(p.getTentativiEffettuati()+1);
             }
-            /** inserire tentativo*/
-            p.setTentativiEffettuati(p.getTentativiEffettuati()+1);
+            /** chiudere partita a partita indovinata */
+            sc.close();
+        }else{
+            //gestisci eccezione
         }
-        /** chiudere partita a partita indovinata */
-
-        sc.close();
     }
 }
