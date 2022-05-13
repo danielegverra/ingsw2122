d2ba2bd5e25d5f2b65d71c4a2dac8b1e0a6228ce
danielegverra <d.guerra6@studenti.uniba.it>
2022-05-13 15:29:19 +0200

    Introdotto controllo in caso di partita abbandonata

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -12,19 +12,29 @@ public class Giocatore {
             Wordle.visualizzaComandi();
             stampaGriglia(p);
             Scanner sc = new Scanner(System.in);
-            while(p.getTentativiEffettuati() < p.getMaxTentativi()){
+            while(p.getTentativiEffettuati() < p.getMaxTentativi() && !partitaFinita){
                 System.out.print("Inserisci il tuo tentativo:\n>");
                 String parolaTentata = sc.nextLine().toUpperCase();
-                while(parolaTentata.length() != p.getParola().length() || !Wordle.parolaValida(parolaTentata)){
-                    System.out.print("La parola da inserire deve avere lunghezza " + p.getParola().length() + " e deve\nessere composta da soli caratteri dell'alfabeto:\n>");
-                    parolaTentata= sc.nextLine().toUpperCase();
-                }
-                p.setGrigliaTentativi(p.getTentativiEffettuati(), parolaTentata);
-                p.setTentativiEffettuati(p.getTentativiEffettuati()+1);
-                stampaGriglia(p);
+                if(parolaTentata.equals("/ABBANDONA")){
+                    partitaFinita = true;
+                } else {
+                    while((parolaTentata.length() != p.getParola().length() || !Wordle.parolaValida(parolaTentata)) && !partitaFinita){
+                        System.out.print("La parola da inserire deve avere lunghezza " + p.getParola().length() + " e deve\nessere composta da soli caratteri dell'alfabeto:\n>");
+                        parolaTentata= sc.nextLine().toUpperCase();
+                        if(parolaTentata.equals("/ABBANDONA")){
+                            partitaFinita = true;
+                        }
+                    }
+                    if(!partitaFinita){
+                        p.setGrigliaTentativi(p.getTentativiEffettuati(), parolaTentata);
+                        p.setTentativiEffettuati(p.getTentativiEffettuati()+1);
+                        stampaGriglia(p);
+                    
 
-                if(parolaTentata.compareTo(p.getParola()) == 0){
-                    /**uscita dalla partita */
+                        if(parolaTentata.compareTo(p.getParola()) == 0){
+                            /**uscita dalla partita */
+                        }
+                    }
                 }
 
             }
