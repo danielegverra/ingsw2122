ab7ac3c94bf35d7eb7a642d6014a15dc5d305038
NicolaLassandro <n.lassandro4@studenti.uniba.it>
2022-05-15 15:30:19 +0200

    Creazione classe Manager

---
+++ src/main/java/it/uniba/app/Manager.java
@@ -0,0 +1,15 @@
+package it.uniba.app;
+
+/**
+ * Questa classe è di tipo 'Boundary'.
+ * Si occupa di ricevere direttamente dall'utente l'input
+ * per poter effettuare le azioni di gioco.
+ */
+public class Manager {
+    
+    /**
+     * METODI
+    */
+
+
+}
