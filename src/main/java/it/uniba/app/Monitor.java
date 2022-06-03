3076b50f8c5e16fe7720bc7ca45f58bbb600f470
adrianafrascella <a.frascella11@studenti.uniba.it>
2022-06-03 12:13:21 +0200

    Creazione classe Monitor

---
+++ src/main/java/it/uniba/app/Monitor.java
@@ -0,0 +1,12 @@
+package it.uniba.app;
+
+import java.util.Scanner;
+
+/**
+ * Questa classe è di tipo 'Boundary'.
+ * Si occupa di gestire la comunicazione in uscita con l'utente.
+ */
+
+public class Monitor {
+    
+}
