12963a2532b0faf62e611f0ed259eef4fd8c7381
adrianafrascella <a.frascella11@studenti.uniba.it>
2022-06-03 12:21:09 +0200

    Spostate funzioni utili alla stampa della griglia in Monitor

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -23,7 +23,7 @@ public class Giocatore {
             System.out.println("\nUna nuova partita sta iniziando!");
             Wordle.visualizzaRegole();
             Wordle.visualizzaComandi();
-            stampaGriglia(p);
+            Monitor.stampaGriglia(p);
             while(p.getTentativiEffettuati() < p.getMaxTentativi() && !partitaFinita){
                 String parolaTentata;
                 parolaTentata= Manager.isTentativo(sc, p, partitaFinita);
@@ -36,7 +36,7 @@ public class Giocatore {
                 else{
                     p.setGrigliaTentativi(p.getTentativiEffettuati(), parolaTentata);
                     p.setTentativiEffettuati(p.getTentativiEffettuati()+1);
-                    stampaGriglia(p);
+                    Monitor.stampaGriglia(p);
                 
                     if(parolaTentata.compareTo(p.getParola()) == 0){
                         partitaFinita = true;
@@ -55,102 +55,5 @@ public class Giocatore {
             System.out.println("Parola segreta mancante.");
         } 
     }
-
-    private static void stampaGriglia(Partita p){
-        /** 
-         * Stampa del separatore superiore
-        */
-        System.out.print(ConsoleColors.WHITE + " ");
-        for (int j = 0; j < p.getParola().length(); j++){
-            System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
-        }
-        System.out.print("\n");
-
-        System.out.println(ConsoleColors.WHITE + "|           WORDLE            |" + ConsoleColors.RESET);
-
-        System.out.print(ConsoleColors.WHITE + " ");
-        for (int j = 0; j < p.getParola().length(); j++){
-            System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
-        }
-        System.out.print("\n");
-        /** 
-         * Stampa delle righe
-        */
-        for(int i = 0; i < p.getMaxTentativi(); i++){
-            if(p.getGrigliaTentativi(i).compareTo("") != 0){                                       //????
-                stampaRigaGriglia(p.getGrigliaTentativi(i), p.getParola());
-            } else {
-                stampaRigaGrigliaVuota(p.getParola().length());
-            }
-            /** 
-             * Stampa del separatore inferiore per ogni riga
-             */
-            System.out.print("\n" + ConsoleColors.WHITE + " ");
-            for (int j = 0; j < p.getParola().length(); j++){
-                System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
-            }
-            System.out.print("\n");
-        }
-
-    }
-
-    private static void stampaRigaGriglia(String parolaTentata, String parolaSegreta){
-        /**
-         * per ogni posizione, 0=grigio 1=verde 2=giallo
-        */
-
-        Integer[] coloriLettereTentate = calcolaColori(parolaTentata, parolaSegreta);       
-        System.out.print(ConsoleColors.WHITE + "|");
-        for(int i = 0; i < parolaSegreta.length(); i++){
-            if(coloriLettereTentate[i] == 0){
-                System.out.print(ConsoleColors.GREY + "  " + parolaTentata.charAt(i)+ "  " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
-            } else if(coloriLettereTentate[i] == 1){
-                System.out.print(ConsoleColors.GREEN + "  " + parolaTentata.charAt(i) + "  "+ ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
-            } else if(coloriLettereTentate[i] == 2){
-                System.out.print(ConsoleColors.YELLOW + "  " + parolaTentata.charAt(i) + "  " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
-            }
-        }
-    }
-
-    private static void stampaRigaGrigliaVuota(int lunghezza){
-        System.out.print(ConsoleColors.WHITE + "|");
-        for(int i = 0; i < lunghezza; i++){
-            System.out.print(ConsoleColors.DARK_WHITE + "     " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
-        }
-    } 
-
-    /**
-     * Questo metodo, sulla base del tentativo effettuato dall'utente, riesce
-     * a indicare ad esso dei suggerimenti visivi, tramite i colori, per poter 
-     * indovinare la parola segreta.
-     */
-    private static Integer[] calcolaColori(String parolaTentata, String parolaSegreta){
-        Integer[] coloriLettereSegrete = new Integer[parolaSegreta.length()];
-        Integer[] coloriLettereTentate = new Integer[parolaSegreta.length()];
-
-        for(int i = 0; i < coloriLettereSegrete.length; i++){
-            coloriLettereTentate[i] = 0;
-            coloriLettereSegrete[i] = 0;
-        }
-        for(int i = 0; i < coloriLettereSegrete.length; i++){
-            if(parolaTentata.charAt(i) == parolaSegreta.charAt(i)){
-                coloriLettereTentate[i] = 1;
-                coloriLettereSegrete[i] = 1;
-            }
-        }
-        for(int i = 0; i < coloriLettereSegrete.length; i++){
-            if(coloriLettereTentate[i] == 0){
-                for(int j = 0; j < coloriLettereSegrete.length && coloriLettereTentate[i] == 0; j++){
-                    if(coloriLettereSegrete[j] == 0 && parolaSegreta.charAt(j) == parolaTentata.charAt(i)){
-                        coloriLettereTentate[i] = 2;
-                        coloriLettereSegrete[j] = 2;
-                    }
-                }
-            }
-        }
-
-        return coloriLettereTentate;
-    }
-
-
+  
 }
--- src/main/java/it/uniba/app/Monitor.java
+++ src/main/java/it/uniba/app/Monitor.java
@@ -8,5 +8,114 @@ import java.util.Scanner;
  */
 
 public class Monitor {
-    
+
+    /**
+     * METODI
+    */
+
+    /**
+     * Questo metodo permette di visualizzare la griglia della partita in corso.
+     */
+    public static void stampaGriglia(Partita p){
+        /** 
+         * Stampa del separatore superiore
+        */
+        System.out.print(ConsoleColors.WHITE + " ");
+        for (int j = 0; j < p.getParola().length(); j++){
+            System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
+        }
+        System.out.print("\n");
+
+        System.out.println(ConsoleColors.WHITE + "|           WORDLE            |" + ConsoleColors.RESET);
+
+        System.out.print(ConsoleColors.WHITE + " ");
+        for (int j = 0; j < p.getParola().length(); j++){
+            System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
+        }
+        System.out.print("\n");
+        /** 
+         * Stampa delle righe
+        */
+        for(int i = 0; i < p.getMaxTentativi(); i++){
+            if(p.getGrigliaTentativi(i).compareTo("") != 0){                                       //????
+                stampaRigaGriglia(p.getGrigliaTentativi(i), p.getParola());
+            } else {
+                stampaRigaGrigliaVuota(p.getParola().length());
+            }
+            /** 
+             * Stampa del separatore inferiore per ogni riga
+             */
+            System.out.print("\n" + ConsoleColors.WHITE + " ");
+            for (int j = 0; j < p.getParola().length(); j++){
+                System.out.print(ConsoleColors.WHITE + "~~~~~ " + ConsoleColors.RESET);
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    /**
+     * Questo metodo viene richiamato in stampaGriglia per stampare il tentativo appena effettuato.
+     */
+    private static void stampaRigaGriglia(String parolaTentata, String parolaSegreta){
+        /**
+         * per ogni posizione, 0=grigio 1=verde 2=giallo
+        */
+
+        Integer[] coloriLettereTentate = calcolaColori(parolaTentata, parolaSegreta);       
+        System.out.print(ConsoleColors.WHITE + "|");
+        for(int i = 0; i < parolaSegreta.length(); i++){
+            if(coloriLettereTentate[i] == 0){
+                System.out.print(ConsoleColors.GREY + "  " + parolaTentata.charAt(i)+ "  " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
+            } else if(coloriLettereTentate[i] == 1){
+                System.out.print(ConsoleColors.GREEN + "  " + parolaTentata.charAt(i) + "  "+ ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
+            } else if(coloriLettereTentate[i] == 2){
+                System.out.print(ConsoleColors.YELLOW + "  " + parolaTentata.charAt(i) + "  " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
+            }
+        }
+    }
+
+    /**
+     * Questo metodo viene richiamato in stampaGriglia per stampare una riga vuota.
+     */
+    private static void stampaRigaGrigliaVuota(int lunghezza){
+        System.out.print(ConsoleColors.WHITE + "|");
+        for(int i = 0; i < lunghezza; i++){
+            System.out.print(ConsoleColors.DARK_WHITE + "     " + ConsoleColors.WHITE + "|" + ConsoleColors.RESET);
+        }
+    } 
+
+    /**
+     * Questo metodo, sulla base del tentativo effettuato dall'utente, riesce
+     * a indicare ad esso dei suggerimenti visivi, tramite i colori, per poter 
+     * indovinare la parola segreta.
+     */
+    private static Integer[] calcolaColori(String parolaTentata, String parolaSegreta){
+        Integer[] coloriLettereSegrete = new Integer[parolaSegreta.length()];
+        Integer[] coloriLettereTentate = new Integer[parolaSegreta.length()];
+
+        for(int i = 0; i < coloriLettereSegrete.length; i++){
+            coloriLettereTentate[i] = 0;
+            coloriLettereSegrete[i] = 0;
+        }
+        for(int i = 0; i < coloriLettereSegrete.length; i++){
+            if(parolaTentata.charAt(i) == parolaSegreta.charAt(i)){
+                coloriLettereTentate[i] = 1;
+                coloriLettereSegrete[i] = 1;
+            }
+        }
+        for(int i = 0; i < coloriLettereSegrete.length; i++){
+            if(coloriLettereTentate[i] == 0){
+                for(int j = 0; j < coloriLettereSegrete.length && coloriLettereTentate[i] == 0; j++){
+                    if(coloriLettereSegrete[j] == 0 && parolaSegreta.charAt(j) == parolaTentata.charAt(i)){
+                        coloriLettereTentate[i] = 2;
+                        coloriLettereSegrete[j] = 2;
+                    }
+                }
+            }
+        }
+
+        return coloriLettereTentate;
+    }
+
 }
