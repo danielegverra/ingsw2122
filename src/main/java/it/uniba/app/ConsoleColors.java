86079bf788842e115ea8912393c2e6a8b22ee661
danielegverra <d.guerra6@studenti.uniba.it>
2022-06-07 10:20:53 +0200

    Corretto lo stile della classe ConsoleColors

--- src/main/java/it/uniba/app/ConsoleColors.java
+++ src/main/java/it/uniba/app/ConsoleColors.java
@@ -3,22 +3,36 @@ package it.uniba.app;
  * Questa classe è di tipo 'noECB'.
  * Si occupa di gestire i colori della matrice di tentativi.
  */
-public class ConsoleColors {
+public final class ConsoleColors {
+
+    private ConsoleColors() {
+
+    }
 
     /**
-     * RESET
+     * RESET.
      */
 
     public static final String RESET = "\033[0m";  // Text Reset
 
     /**
-     * COLORS
+     * GREY.
      */
-    
-    public static final String GREY = "\033[48;2;128;124;124m";                     // GREY
-    public static final String GREEN = "\033[48;2;108;169;104m";                    // GREEN
-    public static final String YELLOW = "\033[48;2;200;180;91m";                    // YELLOW  
-    public static final String WHITE = "\033[48;2;255;255;255m";                    // WHITE
-    public static final String DARK_WHITE = "\033[48;2;210;210;210m";               // DARK_WHITE
-    
+    public static final String GREY = "\033[48;2;128;124;124m";
+    /**
+     * GREEN.
+     */
+    public static final String GREEN = "\033[48;2;108;169;104m";
+    /**
+     * YELLOW.
+     */
+    public static final String YELLOW = "\033[48;2;200;180;91m";
+    /**
+     * WHITE.
+     */
+    public static final String WHITE = "\033[48;2;255;255;255m";
+    /**
+     * DARK_WHITE.
+     */
+    public static final String DARK_WHITE = "\033[48;2;210;210;210m";
 }
