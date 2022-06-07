1080e8ec7c9bc70176f43f20db326a3a71f3bcb1
danielegverra <d.guerra6@studenti.uniba.it>
2022-06-07 10:26:51 +0200

    Corretto lo stile della classe Paroliere

--- src/main/java/it/uniba/app/Paroliere.java
+++ src/main/java/it/uniba/app/Paroliere.java
@@ -4,16 +4,18 @@ package it.uniba.app;
  * Questa classe è di tipo 'Control'.
  * Si occupa di gestire le azioni che può svolgere il paroliere.
  */
-public class Paroliere {
-    /**
-     * METODI
-     */
-    
+public final class Paroliere {
+
+    private Paroliere() {
+
+    };
+
     /**
      * Questo metodo permette al paroliere di cambiare la parola segreta
      * da indovinare.
+     * @param s è la parola segreta da indovinare che deve essere impostata
      */
-    public static void impostaParolaSegreta(String s){
+    public static void impostaParolaSegreta(final String s) {
         Wordle.setParolaSegreta(s);
     }
 
@@ -21,9 +23,9 @@ public class Paroliere {
      * Questo metodo permette al paroliere di visualizzare la parola
      * segreta corrente.
      */
-    public static void  visualizzaParola(){
+    public static void  visualizzaParola() {
 
-        if(!Wordle.getParolaSegreta().equals("")){
+        if (!Wordle.getParolaSegreta().equals("")) {
             Monitor.messaggi("rivelaparola");
         } else {
             Monitor.messaggi("nonesisteparola");
